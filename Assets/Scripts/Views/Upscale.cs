12b533fa7905c0fce96f376dba8cd5d112423114
Carl Corbeil <ca.corbeil@gmail.com>
2020-02-01 15:04:01 -0500

    RT + Downscale Upscale
    UpScale DownScale (not working)

---
+++ Assets/Scripts/Views/Upscale.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Upscale : MonoBehaviour
+{
+    private static readonly Vector2 UpScale = new Vector2(2f, 2f);
+    
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, UpScale, Vector2.zero);
+    }
+}
