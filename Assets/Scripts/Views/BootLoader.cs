a188fae1d6c1af4dcadba8c3d57ecf4f6fe64e24
Carl Corbeil <ca.corbeil@gmail.com>
2020-02-01 14:09:28 -0500

    LoadScene
    Loads the Game Scene and sets it up in the viewport

---
+++ Assets/Scripts/Views/BootLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BootLoader : MonoBehaviour
+{
+    public string _brainScene = "BrainScene";
+    
+    private void Awake()
+    {
+        var asyncOp = SceneManager.LoadSceneAsync(_brainScene, LoadSceneMode.Additive);
+    }
+}
