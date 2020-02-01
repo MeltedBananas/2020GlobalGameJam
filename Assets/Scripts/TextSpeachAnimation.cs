f91a5ca241017ad4de825934a9bfbcc842caa2c2
tyl3n <antoine.mcnabb@hotmail.com>
2020-02-01 12:52:30 -0500

    Text animation

---
+++ Assets/Scripts/TextSpeachAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextSpeachAnimation : MonoBehaviour
+{
+    public delayBetweenLetters = 0.5;
+    public StringToWrite = "Bonjour la maison est rouge";
+    private OutputText = "";
+    public MaxBubbleTextSize = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delayBetweenLetters -= Time.DeltaTime;
+    }
+}
