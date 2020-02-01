e45bb6607879bf0885b372921b8a2f313b6209a2
Carl Corbeil <ca.corbeil@gmail.com>
2020-02-01 16:43:02 -0500

    Added Client Bill
    With animation

---
+++ Assets/Scripts/Views/Animate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Animate : MonoBehaviour
+{
+    [SerializeField] private Image _image = null;
+    [SerializeField] private Texture _spriteSheet = null;
+    [SerializeField] private float _secondsPerSheet = 0.5f;
+
+    private Sprite _initialSprite = null;
+    private Sprite[] _sprites;
+    private bool _isAnimated = false;
+    private Coroutine _animatedCoroutine;
+    private int _indexSprites = 0;
+    
+    private void Awake()
+    {
+        _initialSprite = _image.sprite;
+        _sprites = Resources.LoadAll<Sprite>(_spriteSheet.name);
+    }
+
+    public void Play()
+    {
+        _isAnimated = true;
+        
+        if (_animatedCoroutine == null)
+            _animatedCoroutine = StartCoroutine(AnimateSprite());
+    }
+
+    private IEnumerator AnimateSprite()
+    {
+        while (_isAnimated)
+        {
+            ++_indexSprites;
+            if (_indexSprites >= _sprites.Length)
+                _indexSprites = 0;
+            _image.sprite = _sprites[_indexSprites];
+            yield return new WaitForSeconds(_secondsPerSheet);   
+        }
+        
+        _image.sprite = _initialSprite;
+        _indexSprites = 0;
+        _animatedCoroutine = null;
+    }
+
+    public void Stop()
+    {
+        _isAnimated = false;
+    }
+}
---
+++ Assets/Scripts/Views/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    [SerializeField] private Animate _eyes = null;
+    [SerializeField] private Animate _mouth = null;
+
+    private void Start()
+    {
+        Invoke(nameof(Talk), 2f);
+    }
+
+    public void Talk()
+    {
+        if (_eyes != null) _eyes.Play();
+        if (_mouth != null) _mouth.Play();
+    }
+
+    public void Shutup()
+    {
+        if (_eyes != null) _eyes.Stop();
+        if (_mouth != null) _mouth.Stop();
+    }
+}
