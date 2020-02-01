5f348aad1714940f4bdbd821c8b8df28823be0a0
Carl Corbeil <ca.corbeil@gmail.com>
2020-02-01 18:45:32 -0500

    Tween
    Added Tween system

--- Assets/Scripts/Views/BootLoader.cs
+++ Assets/Scripts/Views/BootLoader.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -16,23 +15,35 @@ public class BootLoader : MonoBehaviour
     public Transform _clientsParent = null;
     public TextSpeachAnimation _speachBubble = null;
 
+    [Header("Speech Bubble Appear")]
+    public float _appearAfterSeconds = 0.25f;
+    public GameObject _speechBubbleImage = null;
+    public LeanTweenType _scaleUpEaseType = LeanTweenType.easeInOutBack;
+    public float _scaleUpTime = 0.4f;
+
     private LevelDefinition _currentLevel = null;
     private Client _currentClient = null;
+    private Vector3 _speechBubbleInitialScale = Vector3.one;
 
     private void Awake()
     {
+        _speachBubble.enabled = false;
+        _speechBubbleImage.SetActive(false);
+        _speechBubbleInitialScale = _speechBubbleImage.transform.localScale;
+        _speechBubbleImage.transform.localScale = Vector3.zero;
+        
         SceneManager.sceneLoaded += OnSceneLoaded;
         SceneManager.LoadSceneAsync(_brainScene, LoadSceneMode.Additive);
-
-        PickRandomLevel();
-        InstantiateLevelObjects();
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
     {
         // Brain Scene was loaded !!
-        var rootGameObjects = scene.GetRootGameObjects();
+        //var rootGameObjects = scene.GetRootGameObjects();
         // Get from root game objects whichever components you need !!
+        
+        PickRandomLevel();
+        InstantiateLevelObjects();
     }
 
     private void PickRandomLevel()
@@ -45,6 +56,14 @@ public class BootLoader : MonoBehaviour
         if (_currentLevel != null)
         {
             _currentClient = Instantiate(_currentLevel.Client.gameObject, _clientsParent).GetComponent<Client>();
+            StartCoroutine(ShowBubbleAfterAFewSeconds(_appearAfterSeconds));
         }
+    }
+
+    private IEnumerator ShowBubbleAfterAFewSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        _speechBubbleImage.SetActive(true);
+        LeanTween.scale(_speechBubbleImage, _speechBubbleInitialScale, _scaleUpTime).setEase(_scaleUpEaseType).setOnComplete(() => _speachBubble.enabled = true);
     }
 }
